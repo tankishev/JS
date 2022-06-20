c5f907b5f9e0bbb4cd3ba0f39427799ed8067453
tankishev <98879302+tankishev@users.noreply.github.com>
2022-06-20 08:09:53 +0300

    update problem 6

--- Advanced/DOM_Introduction_Excersise/06. Format the Text/solution.js
+++ Advanced/DOM_Introduction_Excersise/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value
+    .split(/\.\s?/)
+    .filter((el) => el.length > 0);
+  const output = document.getElementById('output');
+  
+  for (let i = 0; 0 < input.length; i += 3){
+    const child = document.createElement('p');
+    child.textContent = input.slice(i, i + 3).join('.');
+    output.appendChild(child);
+  }
 }
