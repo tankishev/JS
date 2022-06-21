bc87e21b9e13c5c8ab7bf16830d66d759baa240e
tankishev <98879302+tankishev@users.noreply.github.com>
2022-06-21 19:45:11 +0300

    Initial commit

--- Advanced/DOM_Manipulation._Lab/02. Delete-From-Table/app.js
+++ Advanced/DOM_Manipulation._Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const searchValue = document.querySelector('[name="email"]').value;
+    const rows = document.querySelectorAll('tbody tr');
+    const result = document.getElementById('result');
+
+    result.value = '';
+    if (searchValue != ''){
+        if (!(rows.innerHTML.includes(searchValue))){
+            result.textContent = 'Not found.';
+        } else {
+            for (const row of rows){
+                if (row.innerHTML.includes(searchValue)){
+                    row.parentElement.removeChild(row);
+                    result.value = 'Deleted.'
+                }
+            }
+        }
+    }
 }
