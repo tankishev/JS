836272631d2c8454ec9ad2d94eda7f9593f776a4
tankishev <98879302+tankishev@users.noreply.github.com>
2022-06-22 18:40:26 +0300

    Update

--- Advanced/DOM_Manipulation._Lab/05. Highlight-Active/app.js
+++ Advanced/DOM_Manipulation._Lab/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+    for (const el of document.querySelectorAll('input')){
+        el.addEventListener('focus', addFocusClass.bind(el.parentElement));
+        el.addEventListener('blur', removeFocusClass.bind(el.parentElement));
+    }
+    function addFocusClass(){
+        this.classList.add('focused');
+    }
+    function removeFocusClass(){
+        this.classList.remove('focused');
+    }
+
 }
--- Advanced/DOM_Manipulation._Lab/06. Dynamic-Validation/app.js
+++ Advanced/DOM_Manipulation._Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+]/
+    let input = document.getElementById(input);
+    input.addEventListener('change', validateEmail);    
+    
+    function validateEmail(){
+        if (input.value.match(pattern)){
+            console.log('ok');
+        } else {
+            console.log('notok')
+        }
+    }
 }
